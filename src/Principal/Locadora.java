939f3e2827ebd798c5fb7611668d5c2993d00711
Rolf Zambon <led479@gmail.com>
2018-04-17 16:26:33 -0300

    dzgffae
    gsyherdy

--- src/Principal/Locadora.java
+++ src/Principal/Locadora.java
@@ -10,6 +10,6 @@ public class Locadora {
         cadastr.addVeiculo("ABC-5325", "Azul", "Ford", "Fiesta", 2000, 40000, true, true, 250.00);
         cadastr.addVeiculo("XXX-4324", "Preto", "Ford", "New Fiesta", 3000, 30000, true, true, 300.00);
         cadastr.getVeiculo("ABC-5325").display();
-        
+        System.out.println(cadastr.getVeiculo("XXX-4324").getPlaca());
     }
 }
